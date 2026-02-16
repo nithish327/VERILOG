8eaa9c554af3f4cb00395391ccdbe6e8aeb150f4
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-02-16 14:56:04 +0530

    Update ring_counter.v

--- SEQUENTIAL CIRCUIT/COUNTER/ring_counter.v
+++ SEQUENTIAL CIRCUIT/COUNTER/ring_counter.v
@@ -1,23 +1,10 @@
-module ring_counter_tb;
-parameter n=7;
-reg clk;
-reg rst;
-wire [3:0]q;
-ring_counter dut (.*);
-always 
-#10 clk = ~clk;
-initial begin 
-  $dumpfile("ring_counter.vcd");
-  $dumpvars(1,ring_counter_tb);
-  $monitor("Time=%0t clk=%b rst=%b | q=%b",$time,clk,rst,q);
-    rst = 1;
-    clk = 0;
-    
-    #15 rst =0;
-    #120;
-    rst = 1;
-    #10;
-    rst = 0;
-  #80$finish;
-end
+module ring_counter #(parameter n=7)(output reg [3:0]q,
+input wire clk, rst
+);
+always @(posedge clk) begin
+    if (rst)
+     q <= 4'b0010;
+    else 
+     q<= {q[2:0],q[3]};
+end 
 endmodule
