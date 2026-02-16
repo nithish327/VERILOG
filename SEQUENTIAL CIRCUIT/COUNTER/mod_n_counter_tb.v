6394e55b3d36e9926df4a8dc8b50233dd3757d71
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-02-16 13:03:04 +0530

    mod_n_counter_tb.v

--- SEQUENTIAL CIRCUIT/COUNTER/mod_n_counter_tb.v
+++ SEQUENTIAL CIRCUIT/COUNTER/mod_n_counter_tb.v
@@ -1,12 +1,23 @@
-module mod_counter #(parameter n=10)(output reg [3:0]count,
-input wire clk, rst
-);
-always @(posedge clk) begin
-    if (rst)
-    count <= 4'b0000;
-    else if (count == n-1)
-    count <= 0;
-    else 
-    count <= count+1;
-end 
+module mod_counter_tb;
+parameter n=10;
+reg clk;
+reg rst;
+wire [3:0]count;
+mod_counter dut (.*);
+always 
+#10 clk = ~clk;
+initial begin 
+  $dumpfile("mod_counter.vcd");
+  $dumpvars(1,mod_counter_tb);
+  $monitor("Time=%0t clk=%b rst=%b | count=%b",$time,clk,rst,count);
+    rst = 1;
+    clk = 0;
+    
+    #15 rst =0;
+  #120;
+    rst = 1;
+    #10;
+    rst = 0;
+  #80$finish;
+end
 endmodule
