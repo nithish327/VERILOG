b4d643c21290c1713c0ff7f39c4940f8e86eccf7
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-02-19 14:48:50 +0530

    Create sr_ff_tb.v

---
+++ SEQUENTIAL CIRCUIT/MASTER_SLAVE/sr_ff_tb.v
@@ -0,0 +1,30 @@
+module tb_sr_ff;
+reg s,r,clk,rst;
+wire q;
+sr_master_slave dut(.q(q),
+    .rst(rst),
+    .s(s),
+    .r(r),
+    .clk(clk));
+always #10 clk = ~clk; 
+initial begin
+  $dumpfile("sr_master_slave.vcd");
+    $dumpvars(0,tb_sr_ff);
+    clk=0;
+    rst=1;
+    s=0;
+    r=0;
+
+    #20 rst= 0;
+    s=0; r=0;#20;
+    s=0; r=1;#20;
+    s=1; r=0;#20;
+    s=1; r=1;#20;
+   
+   
+    #40 $finish;
+end
+initial begin
+$monitor("Time=%0t rst=%b | clk=%b | s=%b | r=%b | q=%b",$time,rst,clk,s,r,q);
+end
+endmodule
