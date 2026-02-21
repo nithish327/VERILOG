4a7cf3b9074b63e2cc3317c3c38539325eec66ea
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-02-21 17:20:14 +0530

    Create gray_to_binary.v

---
+++ COMBINATIONAL_CIRCUIT/CODE_CONVERTER/gray_to_binary.v
@@ -0,0 +1,12 @@
+module gray_to_binary(input wire [3:0]gray,
+                      output reg [3:0]binary
+               );
+  always @(*) begin
+    binary[3] = gray[3];
+    binary[2] = binary[3] ^ gray[2];
+    binary[1] = binary[2] ^ gray[1];
+    binary[0] = binary[1] ^ gray[0];
+  end 
+  endmodule
+    
+    
